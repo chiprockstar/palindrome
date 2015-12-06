819a2ff39dafbcb5bdb0f684ede273f9a47a3884
Chip Self <chipself9@gmail.com>
2015-12-05 22:20:02 -0500

    refactored solution

--- palindrome.rb
+++ palindrome.rb
@@ -1,5 +1,4 @@
 class Palindrome
-
   def palindrome?(word)
     word.downcase!
     reverse_word = ""
@@ -8,14 +7,10 @@ class Palindrome
     b = 0
     while a > 0
       a -= 1
-      if word[a].ord >= 97 && word[a].ord <= 122
-        reverse_word << word[a]
-      end
-      if word[b].ord >= 97 && word[b].ord <= 122
-        original_word_stripped << word[b]
-      end
+      original_word_stripped << word[b] if word[b].ord >= 97 && word[b].ord <= 122
+      reverse_word << word[a]           if word[a].ord >= 97 && word[a].ord <= 122
       b += 1
     end
-    original_word_stripped == reverse_word ? true : false
+    original_word_stripped == reverse_word
   end
 end
